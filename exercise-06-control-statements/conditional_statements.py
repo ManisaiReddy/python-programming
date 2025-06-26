80b7923844085243f78b65b4230cebc93079031d
Manisai reddy <manisaisaduvala21@gmai.com>
2025-06-26 12:35:36 +0530

    added control statements

---
+++ exercise-06-control-statements/conditional_statements.py
@@ -0,0 +1,48 @@
+# Conditional statements
+"""
+conditional statements are used for decision making and execute the program when they are true
+They are:
+if statement
+else statement
+elif statement
+nested if statements
+"""
+
+# if statements
+"""
+if statements are used for decision making and executes the block of code inside it when the condition is true
+
+"""
+
+x = 10
+if x>5:
+    print("Hey there 10 is greater than 5")
+    
+    
+# else statement
+
+"""
+else statement is used as a fallback action for the if statement
+when we need possible outcome we use it
+"""
+
+if x%2 == 0:
+    print(f"{x} is a even number")
+else:
+    print(f"{x} is not an even number i.e odd")
+
+# elif
+
+"""
+elif stands for else if
+It is used to check the multiple conditions within a block to get desired outcome
+It is more cleaner avoiding the nested if statements
+"""
+
+grade = 70
+if grade<= 60:
+    print("Got B+")
+elif grade == 70:
+    print("Got A+")
+elif grade >= 70:
+    print("Got O")
